6164dc4c4c22f7795ddcff86fd82a3e91380e270
ecn42 <151956146+ecn42@users.noreply.github.com>
2024-04-24 12:43:55 -0300

    Update Dash_Ceres_Capital.py

--- Dash_Ceres_Capital.py
+++ Dash_Ceres_Capital.py
@@ -5,7 +5,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 ##Configuração da página
-st.set_page_config(page_title='Dashboard Ceres Capital', page_icon=':corn:' , layout="wide", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title='Dashboard Ceres Capital-2', page_icon=':corn:' , layout="wide", initial_sidebar_state="auto", menu_items=None)
 
 ##Importando os dados com Funções:
 
