097bcae140109a8fa334c43c8d95efd22bae3798
ecn42 <151956146+ecn42@users.noreply.github.com>
2024-05-06 15:02:47 -0300

    asset allocation

---
+++ pages/4_Carteira.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.set_page_config(page_title='Asset Allocation', page_icon=':graph:' , layout="wide", initial_sidebar_state="auto", menu_items=None)
+# Load the data
+df = pd.read_excel('aa/data.xlsx')
+col1, col2 = st.columns(2)
+# Get the unique profiles
+profiles = df['Perfil'].unique()
+
+with col1:
+    # Create a dropdown menu for profiles
+    st.header('Asset Allocation Sugerido:')
+    selected_profile = st.selectbox('Select a profile', profiles)
+
+    # Filter the dataframe based on the selected profile and non-zero allocation
+    df_filtered = df[(df['Perfil'] == selected_profile) & (df['Alocação'] != 0)]
+
+    # Group by 'Ativo' and sum 'Alocação'
+    df_grouped = df_filtered.groupby('Ativo')['Alocação'].sum().reset_index()
+
+    # Create a pie chart with plotly
+    fig = px.pie(df_grouped, values='Alocação', names='Ativo', title='Asset Allocation sugerida para perfil ' + selected_profile)
+
+    st.plotly_chart(fig)
+
+# Load the suggestions data from 'ativos.xlsx'
+df_suggestions = pd.read_excel('aa/ativos.xlsx')
+
+# Filter the suggestions data based on the selected profile
+df_suggestions = df_suggestions[df_suggestions['Perfil'] == selected_profile]
+
+# Get the unique 'Carteira' in the suggestions data
+carteiras = df_suggestions['Carteira'].unique()
+
+with col2:
+    # Display each 'Carteira' in a separate column
+    for i in range(len(carteiras)):
+        st.header(carteiras[i])
+        df_to_display = df_suggestions[df_suggestions['Carteira'] == carteiras[i]]
+        df_to_display = df_to_display.iloc[:, :-2]  # Drop the last column
+
+        # Check if 'Lâmina' column exists in the dataframe
+        if 'Lâmina' in df_to_display.columns:
+            # Convert the 'Lâmina' column to markdown links
+            df_to_display['Lâmina'] = df_to_display['Lâmina'].apply(lambda x: f'Link' if pd.notnull(x) else "")
+        st.dataframe(df_to_display, hide_index=True)
